b0dd4f1d5c477622a18a1e6389a8fd59a4ed5fbe
Gabriel La Torre <soy@latorregabriel.com>
2014-11-23 14:34:32 -0300

    now we stored emails

--- backend/registerUser.php
+++ backend/registerUser.php
@@ -1,7 +1,7 @@
 <?php
 //print json_encode($_SERVER['HTTP_REFERER']);
 $email = $_GET['email'];
-$myfile = fopen("usuarios.txt", "w");
+$myfile = fopen("usuarios.txt", "a");
 
 fwrite($myfile, $email . ";\n");
 fclose($myfile);
